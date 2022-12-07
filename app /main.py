fa70e5efa85cc216e2808488816c617490593d2b
堤祐貴 <ytsutsumi@fab.pasona.tech>
2022-12-07 15:49:02 +0900

    rabbitmq削除

--- app /main.py
+++ app /main.py
@@ -6,8 +6,6 @@ import boto3
 import shutil
 from os.path import abspath
 import os
-import pika
-import json
 
 app = FastAPI()
 
@@ -76,27 +74,3 @@ async def create_upload_file_minio(file: UploadFile):
 
     return {"filename": file.filename,
             "contents": contents}
-
-# RabbitMQ用
-@app.get("/add-job/{message}")
-def add_job(message: str):
-    # RabbitMQサーバと接続（ホスト名にはコンテナ名を指定しているが，Dockerを使ってない場合はIPアドレスを指定）
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host="rabbitmq"))
-    # チャンネルの確立
-    channel = connection.channel()
-    # メッセージを格納するためのキュー(task_queue)を作成
-    channel.queue_declare(queue="task_queue", durable=True)
-    # メッセージをキュー(task_queue)に格納
-    channel.basic_publish(
-        exchange="",
-        routing_key="task_queue",
-        body=message,
-        properties=pika.BasicProperties(
-            delivery_mode=2,  # メッセージの永続化
-        ))
-    # 接続のクローズ及びメッセージが配信されたことを確認
-    connection.close()
-
-    return {"send": message}
-
